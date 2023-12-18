13cda96f6579ae35daacf7a9d05fd0b25dab2b5d
BatmanovSergeydotnet <xoolly@mail.ru>
2023-12-18 21:45:58 +0300

    Commit HW5.1

--- src/test/java/org/max/seminar/spoon/ClassifyCuisineTest.java
+++ src/test/java/org/max/seminar/spoon/ClassifyCuisineTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.max.home.spoon.AbstractTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
